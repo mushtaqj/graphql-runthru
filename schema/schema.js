859d336f0e64d79fbda35aa261306d7b5ed0f24c
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-03-28 00:42:06 +0530

    adds author resolver and new books

--- schema/schema.js
+++ schema/schema.js
@@ -10,9 +10,12 @@ const _ = require("lodash");
 //Dummy data
 
 const books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 const authors = [
@@ -27,6 +30,14 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve: (parent, args) => {
+        const { authorId } = parent;
+
+        return _.find(authors, {id : authorId});
+      },
+    },
   }),
 });
 
