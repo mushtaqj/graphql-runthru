703b198f91ec4ed08a79adb95def85efd31a13de
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-04-11 01:37:37 +0530

    add book mutation

--- frontend/src/components/AddBook.js
+++ frontend/src/components/AddBook.js
@@ -1,4 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { useState } from "react";
 
 const getAuthorsQuery = gql`
   {
@@ -9,27 +10,57 @@ const getAuthorsQuery = gql`
   }
 `;
 
-const AddBook = () => {
+const addBookMutation = gql`
+  mutation addBook($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      name
+      id
+    }
+  }
+`;
+
+const AddBook = (props) => {
+  const [state, setState] = useState({
+    name: "",
+    genre: "",
+    authorId: "",
+  });
+
   const { loading, error, data } = useQuery(getAuthorsQuery);
+  const [addBook] = useMutation(addBookMutation);
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    addBook({ variables: { ...state } });
+  };
+
+  const mutateBook = (changedBookState) => {
+    setState({ ...state, ...changedBookState });
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-
   const { authors } = data;
 
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={submitForm}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => mutateBook({ name: e.target.value })}
+        />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => mutateBook({ genre: e.target.value })}
+        />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select onChange={(e) => mutateBook({ authorId: e.target.value })}>
           <option>Select author</option>
           {authors.map((author) => (
             <option key={author.id} value={author.id}>
